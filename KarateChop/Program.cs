d9e5029e51896856caf7d999b6cd18ecd6c37584
Geir Sagberg <geir.sagberg@gmail.com>
2013-08-30 08:41:47 +0200

    First commit

---
+++ KarateChop/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarateChop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int EasyChop(int value, int[] ints)
+        {
+            return Array.IndexOf(ints, value);
+        }
+
+        public static int IterativeChop(int value, int[] ints)
+        {
+            int length = ints.Length;
+            if (length == 0)
+                return -1;
+
+            int upper = length, lower = 0, i = length / 2;
+
+            while (true)
+            {
+                int compared = value.CompareTo(ints[i]);
+                if (compared == 0)
+                    return i;
+                if (i == upper || i == lower)
+                    return -1;
+                if (compared < 0)
+                {
+                    upper = i;
+                    i = (i - lower) / 2;
+                }
+                else
+                {
+                    lower = i;
+                    i = (i + upper) / 2;
+                }
+            }
+        }
+
+        public static int RecursiveChop(int value, int[] ints)
+        {
+            int length = ints.Length;
+            if (length == 0)
+                return -1;
+
+            int i = length / 2;
+            int middle = ints[i];
+            int compared = value.CompareTo(middle);
+            if (compared == 0)
+                return i;
+            if (compared < 0)
+            {
+                int[] firstHalf = ints.Take(i).ToArray();
+                return RecursiveChop(value, firstHalf);
+            }
+            int[] secondHalf = ints.Skip(i + 1).ToArray();
+            int result = RecursiveChop(value, secondHalf);
+            if (result == -1) return -1;
+            return i + result + 1;
+        }
+    }
+}
---
+++ KarateChopTest/Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using KarateChop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KarateChopTest
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestEasyChop()
+        {
+            TestImplementation(Program.EasyChop);
+        }
+
+        [TestMethod]
+        public void TestIterativeChop()
+        {
+            TestImplementation(Program.IterativeChop);
+        }
+        [TestMethod]
+        public void TestRecursiveChop()
+        {
+            TestImplementation(Program.RecursiveChop);
+        }
+
+        private static void TestImplementation(Func<int, int[], int> chop)
+        {
+            Assert.AreEqual(-1, chop(3, new int[] {}));
+            Assert.AreEqual(-1, chop(3, new[] {1}));
+            Assert.AreEqual(0, chop(1, new[] {1}));
+
+
+            Assert.AreEqual(0, chop(1, new[] {1, 3, 5}));
+            Assert.AreEqual(1, chop(3, new[] {1, 3, 5}));
+            Assert.AreEqual(2, chop(5, new[] {1, 3, 5}));
+            Assert.AreEqual(-1, chop(0, new[] {1, 3, 5}));
+            Assert.AreEqual(-1, chop(2, new[] {1, 3, 5}));
+            Assert.AreEqual(-1, chop(4, new[] {1, 3, 5}));
+            Assert.AreEqual(-1, chop(6, new[] {1, 3, 5}));
+
+
+            Assert.AreEqual(0, chop(1, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(1, chop(3, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(2, chop(5, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(3, chop(7, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(-1, chop(0, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(-1, chop(2, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(-1, chop(4, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(-1, chop(6, new[] {1, 3, 5, 7}));
+            Assert.AreEqual(-1, chop(8, new[] {1, 3, 5, 7}));
+        }
+    }
+}
